29367d33ae89ee53a9b9ee5c460f55b18fb043e5
Ashish <ashish02.aks@gmail.com>
2026-07-21 19:05:19 +0530

    dynamic port number

--- index.js
+++ index.js
@@ -116,5 +116,5 @@ io.on("connection", (socket) => {
     console.log("User disconnected:", socket.id);
   });
 });
-
-server.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
